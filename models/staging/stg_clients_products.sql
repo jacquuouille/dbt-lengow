ed9a6ca14178b0f510fd515452a5f48cdf6a0e66
jacquuouille <jacqueshervochon@gmail.com>
2026-02-26 12:07:06 +0100

    adding quote false

--- models/staging/stg_clients_products.sql
+++ models/staging/stg_clients_products.sql
@@ -8,9 +8,9 @@ with source as (
         id as client_product_id,
         title as client_product_title,
         price,
-        cast(stock_availability as boolean) as stock_availability,
+        stock_availability,
         main_category,
-        cast(is_active as boolean) as is_active,
+        is_active,
         cast(updated_at as timestamp) as updated_at
     from source
 
